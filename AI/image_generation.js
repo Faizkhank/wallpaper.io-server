15426c75916e20681f648ed56a989b01a463c6dc
faiz khan <fine7production@gmail.com>
2023-04-09 23:45:53 +0530

    new updates

---
+++ AI/image_generation.js
@@ -0,0 +1,41 @@
+const Router = require("express").Router();
+const { Configuration, OpenAIApi } = require("openai");
+const fs = require("fs");
+const configuration = new Configuration({
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+Router.post("/gen", async (req, res) => {
+  const { prompt } = req.body;
+  console.log(prompt);
+  const imageSize = "1024x1024";
+
+  try {
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: imageSize,
+    });
+
+    const imageUrl = response.data.data[0].url;
+
+    res.status(200).json({
+      success: true,
+      data: imageUrl,
+    });
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
+    }
+
+    res.status(400).json({
+      success: false,
+      error: "The image could not be generated",
+    });
+  }
+});
+module.exports = Router;
