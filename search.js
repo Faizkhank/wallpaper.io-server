1e660a5f4c711472d348aba884d4048c997529a2
faiz khan <fine7production@gmail.com>
2023-04-11 00:35:25 +0530

    c

--- search.js
+++ search.js
@@ -34,6 +34,38 @@ Router.get("/api/search", async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 });
+Router.get("/api/page_0/search", async (req, res) => {
+  const query = req.query.q;
+
+  try {
+    const results = await ImageUpload.find({
+      $or: [
+        { Name: { $regex: query, $options: "i" } }, // search by name
+        { Filename: { $regex: query, $options: "i" } }, // search by description
+        { tags: { $regex: query, $options: "i" } }, // search by tags
+      ],
+    }).limit(10);
+    if (results) {
+      var result = [];
+      results.forEach((items) => {
+        const obj = {
+          _id: items._id,
+          Name: items.Name,
+          Filename: items.Filename,
+          UserURL: items.UserURL,
+          UploaderID: items.UploaderID,
+          Url: items.Url,
+          Likes: items.Likes.length,
+        };
+        result.push(obj);
+      });
+      res.send(result);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 Router.get("/data", async (req, res) => {
   const query = req.query.page;
   try {
