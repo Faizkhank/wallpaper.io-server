fdbc8cbd77151fcfcc842793f62e35962621db67
faiz khan <fine7production@gmail.com>
2023-06-06 22:45:19 +0530

    commit

--- follow.js
+++ follow.js
@@ -1,38 +1,41 @@
 const Router = require("express").Router();
 const User = require("./Models/UserSchema");
 Router.put("/follow/:userID/:followID", async (req, res) => {
-  const userID = req.params.userID;
-  const id = req.params.followID;
-  const Usersave = await User.findById(userID);
-  const found = Usersave.followers.some((item) => item.followers === id);
-  if (found) {
-    // User is not following, so add follower
-    await User.findOneAndUpdate(
-      { _id: userID },
-      {
-        $pull: {
-          followers: { followers: id },
-        },
-        $inc: { followerscount: -1 },
-      }
-    );
-    res.send(false);
-  } else {
-    // User is following, so remove follower
-    await User.findOneAndUpdate(
-      { _id: userID },
-      {
-        $push: {
-          followers: { followers: id },
-        },
-        $inc: { followerscount: 1 },
-      }
-    );
+  try {
+    const userID = req.params.userID;
+    const id = req.params.followID;
+    const Usersave = await User.findById(userID);
+    const found = Usersave.followers.some((item) => item.followers === id);
+    if (found) {
+      // User is not following, so add follower
+      await User.findOneAndUpdate(
+        { _id: userID },
+        {
+          $pull: {
+            followers: { followers: id },
+          },
+          $inc: { followerscount: -1 },
+        }
+      );
+      res.send(false);
+    } else {
+      // User is following, so remove follower
+      await User.findOneAndUpdate(
+        { _id: userID },
+        {
+          $push: {
+            followers: { followers: id },
+          },
+          $inc: { followerscount: 1 },
+        }
+      );
 
-    res.send(true);
+      res.send(true);
+    }
+  } catch (error) {
+    console.error("An error occurred:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
-
-  res.status(502);
 });
 Router.get("/checkfollow/:followID/:followerID", async (req, res) => {
   const userid = req.params.followerID;
